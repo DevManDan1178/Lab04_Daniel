ec08dd590ab57cbd298ae6a742a22afd9684ff67
Daniel Orejuela Liu <6323612@edu.vaniercollege.qc.ca>
2025-09-22 10:44:55 -0400

    updated calculation method / input validation

--- src/lab05_daniel/Lab05_Daniel.java
+++ src/lab05_daniel/Lab05_Daniel.java
@@ -54,7 +54,7 @@ public class Lab05_Daniel extends Application{
                 try {
                     double entry = Double.parseDouble(field.getText());
                     if (entry < 0) {
-                        priceCalcParams[thisIndex] = 0;
+                        field.setText(priceCalcParams[thisIndex] <= 0? "" : String.format("%.2f", priceCalcParams[thisIndex]));
                         return;
                     }
                     //Since is money, round at cents (1/100)
@@ -64,7 +64,8 @@ public class Lab05_Daniel extends Application{
                     allowableExpensesLbl.setText(String.format("Allowable expenses: %.2f$", results[1]));
                     excessOrSavedLbl.setText(String.format("%s: %.2f$", results[2] < 0? "Saved" : "Excess", Math.abs(results[2])));
                 } catch (Exception e) {
-                    priceCalcParams[thisIndex] = 0;
+                    //Reset text if invalid characters to previous value or empty if invalid value
+                    field.setText(priceCalcParams[thisIndex] <= 0? "" : String.format("%.2f", priceCalcParams[thisIndex]));
                 }
             });
         }
@@ -91,10 +92,11 @@ public class Lab05_Daniel extends Application{
         return calcTravelExpenses((int) args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
     }
     
-    private double[] calcTravelExpenses(int dayCount, double airfare, double carRental, double milesDriven, double parkingFees, double taxiCharges, double registrationFees, double lodgingCharges)  {
+    private double[] calcTravelExpenses(int dayCount, double airfare, double carRental, double milesDriven, double parkingFees, double taxiCharges, double registrationFees, double nightlyLodgingCharges)  {
+        double dayCountDbl = (double) dayCount;
         //not counting miles driven since there is no stated fee for miles driven
-        double totalExpenses = airfare + carRental + parkingFees + taxiCharges + registrationFees + lodgingCharges * (double) dayCount;
-        double allowableExpenses = (double) dayCount * (DAILY_MEALS + DAILY_TAXI + DAILY_PARKING + DAILY_LODGING) + milesDriven * PER_MILE;
+        double totalExpenses = airfare + carRental + parkingFees + taxiCharges + registrationFees + nightlyLodgingCharges * (double) dayCount;
+        double allowableExpenses = dayCountDbl * (DAILY_MEALS + Math.min(DAILY_TAXI, taxiCharges/dayCountDbl) + Math.min(DAILY_PARKING, parkingFees/dayCountDbl) + Math.min(DAILY_LODGING, nightlyLodgingCharges)) + milesDriven * PER_MILE;
         double excessExpenses = totalExpenses - allowableExpenses; //if under 0, this becomes total saved
         return new double[] {totalExpenses, allowableExpenses, excessExpenses};
     }
