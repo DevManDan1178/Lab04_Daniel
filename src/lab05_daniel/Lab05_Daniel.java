147145fcbab0679b42a6fd3db015f04f7c3650a6
Daniel Orejuela Liu <6323612@edu.vaniercollege.qc.ca>
2025-09-22 08:33:19 -0400

    comm 1

---
+++ src/lab05_daniel/Lab05_Daniel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab05_daniel;
+import javafx.application.Application;
+
+/**
+ *
+ * @author 6323612
+ */
+public class Lab05_Daniel extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+}
